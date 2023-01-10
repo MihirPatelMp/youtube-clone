7896c49daf00bf8da5c3bcf78fc606bec03582b5
Mihir Patel <mp80485@gmail.com>
2023-01-10 10:18:24 +0530

    Changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
@@ -7,11 +7,10 @@ import {
   VideoDetail,
   ChannelDetail,
   SearchFeed,
-  SearchBar,
 } from "./components";
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
@@ -21,7 +20,7 @@ const App = () => (
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
